52451d2e74e720c3b144683951466ba9e54c9675
VashchenkoArtem <artemvash201010@gmail.com>
2025-06-26 20:59:33 +0300

    Fixing bugs

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'daphne',
+    'mysql.connector.django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
